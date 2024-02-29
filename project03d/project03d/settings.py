9fd1be9386b227b37962787bcf960c64eb247535
admin <philipp.msc@gmail.com>
2024-02-29 14:02:29 +0300

    mail2

--- project03d/project03d/settings.py
+++ project03d/project03d/settings.py
@@ -171,7 +171,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "neckcrank"
-EMAIL_HOST_PASSWORD = "azzkodgjhpqdafzx"
+EMAIL_HOST_PASSWORD = "*****"
 # EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
